e0a9d94935e31339fd5fe2cc8544e9a3db98c030
sachinbm40 <95728394+sachinbm40@users.noreply.github.com>
2022-01-14 14:59:33 +0530

    Delete add.sh

--- add.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#test
-echo "enter the integer value for num1"
-read num1
-echo "enter the integer value for num2 other then num1"
-read num2
-sum=`expr $num1 + $num2`
-echo "the sum of two numbers is $sum"
